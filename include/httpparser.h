b2793bda3764070a93111019875eb035c3fdcdc7
Telephone <telephone.tan@qq.com>
2021-05-02 01:30:21 +0800

    add next_http_message() in httpparser

--- include/httpparser.h
+++ include/httpparser.h
@@ -3,12 +3,51 @@
 
 #include <stddef.h>
 
+#include <macros.h>
+#ifdef LOGME_WINDOWS
+#define CASE_INSENSITIVE_STRSTR
+#define CASE_INSENSITIVE_STRSTR_WINDOWS
+#elif defined(LOGME_GCC)
+#define CASE_INSENSITIVE_STRSTR
+#define CASE_INSENSITIVE_STRSTR_GCC
+#endif // LOGME_WINDOWS
+
+#define MAX_HTTP_HEADERS_LENGTH 28672
+
+typedef enum HttpMethod {
+	GET = 0, POST, HEAD, PUT, DELETE_, CONNECT, OPTIONS, TRACE, PATCH, INVALID_METHOD
+} HttpMethod;
+
+typedef char(GENERATOR_FUNCTION_TYPE)(void*, int*);
+
+typedef void GENERATOR_PARAM_TYPE;
+
 // 此函数从头查找第一个符合的子字符串。有两种工作模式：流模式和固定字符串模式。
+// 如果工作在流模式下，函数返回时，如果 call_time 不是 NULL，字符生成器调用成功的次数存储在参数 call_time 指向的变量中。
+// 如果工作在固定字符串模式下，如果 call_time 不是 NULL，那么函数运行时可能会修改参数 call_time 指向的变量的值，但写入 call_time 的值没有意义。
 // 返回值：
 // -1 ：没找到
 // -2 : 动态内存分配失败
-// -3 : 字符生成器调用失败。字符生成器调用成功的次数存储在参数 call_time 指向的变量中
+// -3 : 字符生成器调用失败
 // >=0 : 找到了。没有错误发生。流模式下返回值是字符生成器被调用的次数；固定字符串模式下返回值是找到的子字符串的最后一个字符的后一个下标（不管是否超出数组界限）
-int find_sub_str(size_t max_call_time, char(*generator)(void*, int*), void* generator_param_p, const char* str, const char* pattern, size_t* call_time, char* generated_buf, size_t generated_buf_len);
+int find_sub_str(size_t max_call_time, GENERATOR_FUNCTION_TYPE* generator, GENERATOR_PARAM_TYPE* generator_param_p, const char* str, const char* pattern, size_t* call_time, char* generated_buf, size_t generated_buf_len
+
+#ifdef CASE_INSENSITIVE_STRSTR
+	, int case_sensitive
+#endif // CASE_INSENSITIVE_STRSTR
+
+);
+
+#ifdef CASE_INSENSITIVE_STRSTR
+// 从流中取出下一个 HTTP 报文，不合法的数据也会被从流中取出，但不合法的数据会被丢弃。提取出的报文以字符串的形式存放于 message_pp 指向的指针指向的一块内存中。
+// 返回值：
+// >= 0 : HTTP 报文的长度（不包括结尾的空字符）
+// -1 : 参数不合法，method_p 是空指针或 message_pp 是空指针
+// -2 : find_sub_str() 动态内存分配失败
+// -3 : 字符生成器 generator 调用失败
+// -4 : 已经检测到合法报文，但尝试开辟一块内存来存放报文时发生了动态内存分配失败
+// 其他负数 : find_sub_str() 返回此错误码
+int next_http_message(HttpMethod* method_p, char** message_pp, GENERATOR_FUNCTION_TYPE* generator, GENERATOR_PARAM_TYPE* generator_param_p);
+#endif // CASE_INSENSITIVE_STRSTR
 
 #endif // !HTTPPARSER
--- src/src1/httpparser.c
+++ src/src1/httpparser.c
@@ -1,12 +1,30 @@
 #include "httpparser.h"
 
+#ifdef CASE_INSENSITIVE_STRSTR_WINDOWS
+#include <Shlwapi.h>
+#include <string.h>
+#elif defined(CASE_INSENSITIVE_STRSTR_GCC)
+#define _GNU_SOURCE
+#include <string.h>
+#else
+#include <string.h>
+#endif // CASE_INSENSITIVE_STRSTR_WINDOWS
+
 #include <stddef.h>
 #include <string.h>
+#include <ctype.h>
 #include <stdlib.h>
 
 #include <vutils.h>
+#include <vlist.h>
 
-static long long last_index_of_str(const char* s, const char* pattern, long long before_index) {
+static long long last_index_of_str(const char* s, const char* pattern, long long before_index
+
+#ifdef CASE_INSENSITIVE_STRSTR
+	, int case_sensitive
+#endif // CASE_INSENSITIVE_STRSTR
+
+) {
 	size_t slen = strlen(s);
 	before_index = (before_index >= 0 ? before_index : 0);
 	const char* before_ptr = s + before_index;
@@ -14,7 +32,22 @@ static long long last_index_of_str(const char* s, const char* pattern, long long
 	const char* pt = s;
 	for (size_t i = 0; i < slen; i++)
 	{
-		const char *next_found = strstr(pt++, pattern);
+		const char* next_found =
+#ifdef CASE_INSENSITIVE_STRSTR
+			case_sensitive ?
+#endif // CASE_INSENSITIVE_STRSTR
+			strstr(pt++, pattern)
+#ifdef CASE_INSENSITIVE_STRSTR
+			:
+#ifdef CASE_INSENSITIVE_STRSTR_WINDOWS
+			StrStrI(pt++, pattern)
+#elif defined(CASE_INSENSITIVE_STRSTR_GCC)
+			strcasestr(pt++, pattern)
+#else
+			strstr(pt++, pattern)
+#endif // CASE_INSENSITIVE_STRSTR_WINDOWS
+#endif // CASE_INSENSITIVE_STRSTR
+			;
 		if (!next_found || next_found >= before_ptr)
 		{
 			break;
@@ -24,16 +57,26 @@ static long long last_index_of_str(const char* s, const char* pattern, long long
 	return found ? (found - s) : -1;
 }
 
-static long long last_index_of_ch(const char* s, char ch, long long before_index) {
+static long long last_index_of_ch(const char* s, char ch, long long before_index
+
+#ifdef CASE_INSENSITIVE_STRSTR
+	, int case_sensitive
+#endif // CASE_INSENSITIVE_STRSTR
+
+) {
 	char substr[2] = {ch, 0};
+#ifdef CASE_INSENSITIVE_STRSTR
+	return last_index_of_str(s, substr, before_index, case_sensitive);
+#else
 	return last_index_of_str(s, substr, before_index);
+#endif // CASE_INSENSITIVE_STRSTR
 }
 
 // generator 可能调用失败。
 // 如果某次 generator 调用失败，那么此次 p_head_index 下标不会增加，此次 buf 也不会被写入，
 // 此次滑动取消（只是取消这一个字符距离的滑动，不是取消所有滑动），同时立即停止滑动。
 // 返回值：成功滑动的距离（字符为单位）
-static size_t bm_shift(size_t* p_head_index, size_t num, char *buf, char(*generator)(void*,int*), void *generator_param_p, char* generated_buf, size_t generated_buf_len, size_t *generated_used_len_p) {
+static size_t bm_shift(size_t* p_head_index, size_t num, char *buf, GENERATOR_FUNCTION_TYPE* generator, GENERATOR_PARAM_TYPE* generator_param_p, char* generated_buf, size_t generated_buf_len, size_t *generated_used_len_p) {
 	if (generator)
 	{
 		for (size_t i = 0; i < num; i++)
@@ -58,10 +101,18 @@ static size_t bm_shift(size_t* p_head_index, size_t num, char *buf, char(*genera
 	return num;
 }
 
-int find_sub_str(size_t max_call_time, char(*generator)(void*,int*), void* generator_param_p, const char *str, const char *pattern, size_t *call_time, char* generated_buf, size_t generated_buf_len) {
+int find_sub_str(size_t max_call_time, GENERATOR_FUNCTION_TYPE* generator, GENERATOR_PARAM_TYPE* generator_param_p, const char* str, const char* pattern, size_t* call_time, char* generated_buf, size_t generated_buf_len
+
+#ifdef CASE_INSENSITIVE_STRSTR
+	, int case_sensitive
+#endif // CASE_INSENSITIVE_STRSTR
+
+) {
 	const size_t plen = strlen(pattern);
 	size_t shifted = 0;
 	size_t generated_used_len = 0;
+	size_t nothing;
+	call_time == NULL ? (call_time = &nothing) : (call_time);
 	max_call_time = (str ? strlen(str) : max_call_time);
 	generator = (str ? NULL : generator);
 	generator_param_p = (str ? NULL : generator_param_p);
@@ -69,6 +120,7 @@ int find_sub_str(size_t max_call_time, char(*generator)(void*,int*), void* gener
 	char* temp = (str ? str : zero_malloc(max_call_time + 1));
 	if (!temp)
 	{
+		*call_time = shifted;
 		return -2;
 	}
 
@@ -82,15 +134,15 @@ int find_sub_str(size_t max_call_time, char(*generator)(void*,int*), void* gener
 	shifted += plen;
 	if (shifted > max_call_time)
 	{
-		if (!str)free(temp);
+		shifted -= plen;
+		if (!str)free(temp); *call_time = shifted;
 		return -1;
 	}
 	size_t a_shift = bm_shift(&head_index, plen, temp, generator, generator_param_p, generated_buf, generated_buf_len, &generated_used_len);
 	if (a_shift < plen)
 	{
 		shifted -= plen; shifted += a_shift;
-		*call_time = shifted;
-		if (!str)free(temp);
+		if (!str)free(temp); *call_time = shifted;
 		return -3;
 	}
 
@@ -101,19 +153,36 @@ loop:;
 		size_t pattern_index = look_back_index - tail_index;
 		char pch = pattern[pattern_index];
 		char tch = temp[look_back_index];
-		if (tch == pch)
+		if (
+#ifdef CASE_INSENSITIVE_STRSTR
+			case_sensitive ?
+#endif // CASE_INSENSITIVE_STRSTR
+			tch == pch
+#ifdef CASE_INSENSITIVE_STRSTR
+			:
+		toupper(tch) == toupper(pch)
+#endif // CASE_INSENSITIVE_STRSTR
+			)
 		{
 			continue;
 		}
 		else
 		{
-			long long left_index_of_tch_in_pattern = last_index_of_ch(pattern, tch, pattern_index);
+			long long left_index_of_tch_in_pattern = last_index_of_ch(pattern, tch, pattern_index
+#ifdef CASE_INSENSITIVE_STRSTR
+				, case_sensitive
+#endif // CASE_INSENSITIVE_STRSTR
+			);
 			size_t shift_len = ((long long)pattern_index) - left_index_of_tch_in_pattern;
 			if (look_back_index != head_index)
 			{
 				char hch = temp[head_index];
 				size_t pattern_index_2 = plen - 1;
-				long long left_index_of_hch_in_pattern = last_index_of_ch(pattern, hch, pattern_index_2);
+				long long left_index_of_hch_in_pattern = last_index_of_ch(pattern, hch, pattern_index_2
+#ifdef CASE_INSENSITIVE_STRSTR
+					, case_sensitive
+#endif // CASE_INSENSITIVE_STRSTR
+				);
 				size_t shift_len_2 = ((long long)pattern_index_2) - left_index_of_hch_in_pattern;
 				if (shift_len_2 > shift_len)
 				{
@@ -123,20 +192,160 @@ loop:;
 			shifted += shift_len;
 			if (shifted > max_call_time)
 			{
-				if (!str)free(temp);
+				shifted -= shift_len;
+				if (!str)free(temp); *call_time = shifted;
 				return -1;
 			}
 			size_t a_shift = bm_shift(&head_index, shift_len, temp, generator, generator_param_p, generated_buf, generated_buf_len, &generated_used_len);
 			if (a_shift < shift_len)
 			{
 				shifted -= shift_len; shifted += a_shift;
-				*call_time = shifted;
-				if (!str)free(temp);
+				if (!str)free(temp); *call_time = shifted;
 				return -3;
 			}
 			goto loop;
 		}
 	}
-	if (!str)free(temp);
+	if (!str)free(temp); *call_time = shifted;
 	return shifted;
 }
+
+#ifdef CASE_INSENSITIVE_STRSTR
+typedef struct generator_wrapper_param {
+	GENERATOR_FUNCTION_TYPE* generator;
+	GENERATOR_PARAM_TYPE* generator_param_p;
+	vlist generated_buffer;
+	long next_read_buffer_index;
+} generator_wrapper_param;
+
+static char generator_wrapper(void* generator_wrapper_param_p, int* continue_flag) {
+	generator_wrapper_param* gwpp = generator_wrapper_param_p;
+	GENERATOR_FUNCTION_TYPE* generator = gwpp->generator;
+	GENERATOR_PARAM_TYPE* gpp = gwpp->generator_param_p;
+	vlist generated_buffer = gwpp->generated_buffer;
+	if (gwpp->next_read_buffer_index >= generated_buffer->size)
+	{
+		char read_ch = generator(gpp, continue_flag);
+		if (*continue_flag)
+		{
+			// 只有 generator 调用成功，才把 generator 的返回字符放入缓冲区
+			generated_buffer->add(generated_buffer, &read_ch);
+			gwpp->next_read_buffer_index = generated_buffer->size;
+		}
+		return read_ch;
+	}
+	else
+	{
+		*continue_flag = 1;
+		return *((const char*)(generated_buffer->get_const(generated_buffer, gwpp->next_read_buffer_index++)));
+	}
+}
+
+int next_http_message(HttpMethod *method_p, char **message_pp, GENERATOR_FUNCTION_TYPE* generator, GENERATOR_PARAM_TYPE* generator_param_p) {
+	if (!method_p || !message_pp)
+	{
+		return -1;
+	}
+	const char *(http_method_names[]) = {
+		"GET", "POST", "HEAD", "PUT", "DELETE", "CONNECT", "OPTIONS", "TRACE", "PATCH"
+	};
+	vlist generated_buffer = make_vlist(sizeof(char));
+	generator_wrapper_param gwp = {
+		.generator = generator,
+		.generator_param_p = generator_param_p,
+		.generated_buffer = generated_buffer,
+		.next_read_buffer_index = 0
+	};
+	int malloc_fail_type = 0;
+again:;
+	*method_p = INVALID_METHOD;
+	for (HttpMethod i = GET; i < INVALID_METHOD; i++)
+	{
+		int method_f_res = find_sub_str(MAX_HTTP_HEADERS_LENGTH, generator_wrapper, &gwp, NULL, http_method_names[i], NULL, NULL, 0, 0);
+		if (method_f_res >= 0)
+		{
+			*method_p = i;
+			int empty_line_f_res = find_sub_str(MAX_HTTP_HEADERS_LENGTH, generator_wrapper, &gwp, NULL, "\r\n\r\n", NULL, NULL, 0, 0);
+			if (empty_line_f_res >= 0)
+			{
+				int m_start_index = method_f_res - strlen(http_method_names[i]);
+				int m_end_index = method_f_res + empty_line_f_res;
+				int m_len = m_end_index - m_start_index;
+				*message_pp = zero_malloc(m_len + 1);
+				if (!(*message_pp))
+				{
+					malloc_fail_type = -4;
+					goto malloc_fail;
+				}
+				for (int m_i = m_start_index, dest_i = 0; m_i < m_end_index; m_i++, dest_i++)
+				{
+					(*message_pp)[dest_i] = *((const char*)(generated_buffer->get_const(generated_buffer, m_i)));
+				}
+				delete_vlist(generated_buffer, &generated_buffer);
+				return m_len;
+			}
+			else if (empty_line_f_res == -1)
+			{
+				goto not_found;
+			}
+			else if (empty_line_f_res == -2)
+			{
+				malloc_fail_type = -2;
+				goto malloc_fail;
+			}
+			else if (empty_line_f_res == -3)
+			{
+				goto generator_fail;
+			}
+			else
+			{
+				method_f_res = empty_line_f_res;
+				goto unknown_error;
+			}
+		}
+		else if (method_f_res == -1)
+		{
+			// not found
+		not_found:;
+			gwp.next_read_buffer_index = 0;
+			continue;
+		}
+		else if (method_f_res == -2)
+		{
+			// malloc fail
+			malloc_fail_type = -2;
+		malloc_fail:;
+			*method_p = INVALID_METHOD;
+			*message_pp = NULL;
+			delete_vlist(generated_buffer, &generated_buffer);
+			return malloc_fail_type;
+		}
+		else if (method_f_res == -3)
+		{
+			// generator() fail
+		generator_fail:;
+			*method_p = INVALID_METHOD;
+			*message_pp = NULL;
+			delete_vlist(generated_buffer, &generated_buffer);
+			return -3;
+		}
+		else
+		{
+			// unknown error
+		unknown_error:;
+			*method_p = INVALID_METHOD;
+			*message_pp = NULL;
+			delete_vlist(generated_buffer, &generated_buffer);
+			return method_f_res;
+		}
+	}
+	if (*method_p == INVALID_METHOD)
+	{
+		generated_buffer->remove(generated_buffer, 0);
+		gwp.next_read_buffer_index = 0;
+		goto again;
+	}
+	delete_vlist(generated_buffer, &generated_buffer);
+	while (1);
+}
+#endif // CASE_INSENSITIVE_STRSTR
