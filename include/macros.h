27a9712b6f0daea2636ec598dfbe63a3b6761039
Telephone <telephone.tan@qq.com>
2021-05-11 20:24:24 +0800

    add some macros in macros.h

--- include/macros.h
+++ include/macros.h
@@ -3,14 +3,18 @@
 
 #ifdef __GNUC__
 #define LOGME_GCC
+#define V_GCC
 #ifdef _WIN32
 #define LOGME_WINDOWS
+#define V_WINDOWS
 #endif // _WIN32
 #endif // __GNUC__
 #ifdef _MSC_VER
 #define LOGME_MSVC
+#define V_MSVC
 #ifdef _WIN32
 #define LOGME_WINDOWS
+#define V_WINDOWS
 #endif // _WIN32
 #endif // _MSC_VER
 
