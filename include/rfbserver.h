f5cdba6175e9dca70c56567d70ea20254809b844
Telephone <telephone.tan@qq.com>
2021-07-26 14:08:50 +0800

    add incomplete library: RFBServer

---
+++ include/rfbserver.h
@@ -0,0 +1,16 @@
+//
+// Created by Telephone on 2021/7/26.
+//
+
+#ifndef EXAMPAPERSYSTEM_RFBSERVER_H
+#define EXAMPAPERSYSTEM_RFBSERVER_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// TODO: add some code here
+
+#ifdef __cplusplus
+}
+#endif
+#endif //EXAMPAPERSYSTEM_RFBSERVER_H
---
+++ src/src1/rfbserver.c
@@ -0,0 +1,14 @@
+//
+// Created by Telephone on 2021/7/26.
+//
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "rfbserver.h"
+
+// TODO: add some code here
+
+#ifdef __cplusplus
+}
+#endif
